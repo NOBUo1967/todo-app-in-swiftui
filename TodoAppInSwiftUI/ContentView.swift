cdbc0a1852303595848d5835a1994a956a176525
Shin H <nobunegi13@gmail.com>
2022-02-18 10:34:44 +0900

    Initial Commit

---
+++ TodoAppInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoAppInSwiftUI
+//
+//  Created by Shinichiro Hirasawa on 2022/02/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoAppInSwiftUI/TodoAppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppInSwiftUIApp.swift
+//  TodoAppInSwiftUI
+//
+//  Created by Shinichiro Hirasawa on 2022/02/18.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
