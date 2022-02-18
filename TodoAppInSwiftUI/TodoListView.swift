000a4620d9baa3a8dae78cc5aa2ba1740e334f23
Shin H <nobunegi13@gmail.com>
2022-02-18 11:12:58 +0900

    initial commit

---
+++ TodoAppInSwiftUI/TodoListView.swift
@@ -0,0 +1,22 @@
+//
+//  TodoListView.swift
+//  TodoAppInSwiftUI
+//
+//  Created by Shinichiro Hirasawa on 2022/02/18.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        VStack {
+            Text("Test")
+        } // VStack
+    } // body
+} // TodoListView
+
+struct TodoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListView()
+    }
+}
